f548021e5cb5c71f609a7ef0fefb313f4769a136
Progyan Bhattacharya <bprogyan@gmail.com>
2017-05-08 20:06:21 +0530

    bash: Added auto completion script (#19)
    * bash: Added auto completion script
    
    Added auto completion script for ease to use
    Fixes: #17
    
    Signed-Off-By: Progyan Bhattacharya <bprogyan@gmail.com>
    
    * bash: Modified install script
    
    Signed-Off-By: Progyan Bhattacharya <bprogyan@gmail.com>

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 #!/usr/bin/env node
-'use strict'
+
+/*
+ * GHB - GitHub on Terminal
+ * Copyright (C) 2017 Progyan Bhattacharya <admin@codeprogyan.me>
+ * Maintained by: Bytes Club (https://bytesclub.github.io)<bytes-club@googlegroups.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict';
 
 const  argv = process.argv,
        fs   = require('fs'),
@@ -24,7 +49,9 @@ ghb -h or ghb --help to see usage details.`
 
 var ghb, index = 0
 
-if ((index = argv.indexOf('-h')) !== -1 || (argv.indexOf('--help')) !== -1) {
+if ((index = argv.indexOf('-v')) !== -1 || (argv.indexOf('--version')) !== -1) {
+	console.log('GHB v1.2.0')
+} else if ((index = argv.indexOf('-h')) !== -1 || (argv.indexOf('--help')) !== -1) {
 	console.log(helpStr)
 } else if ((index = argv.indexOf('init')) !== -1) {
 	if (typeof argv[index + 1] === 'string') {
---
+++ ghb-auto.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# bash/zsh completion support for GHB.
+#
+# GHB - GitHub on Terminal
+# Copyright (C) 2017 Progyan Bhattacharya <admin@codeprogyan.me>
+# Maintained by: Bytes Club (https://bytesclub.github.io)<bytes-club@googlegroups.com>
+# Distributed under MIT License.
+
+_ghb() {
+    local cur prev opt
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    readonly -a opts=("-h" "--help" "-v" "--version" "init" "status" "issues" "pulls")
+    opt=''
+
+    for i in "${opts[@]}"
+    do
+        if [[ $i == "${cur}"* ]]; then
+            opt="$opt $i"
+        fi
+    done
+    COMPREPLY=( $(compgen -W "${opt}" -- ${cur}) )
+    return 0
+}
+complete -F _ghb ghb
