a5c577ff1f41da810841f7f51b9c7bc0abf37059
Pragyan Bhattacharya <bprogyan@gmail.com>
2017-05-10 09:43:19 +0530

    Auto: Added additional installation script
    This script will move auto complete script to
    bash directory and execute it
    
    Signed-Off-By: Progyan Bhattacharya <bprogyan@gmail.com>

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Additional Installation script for GHB.
+#
+# GHB - GitHub on Terminal
+# Copyright (C) 2017 Progyan Bhattacharya <admin@codeprogyan.me>
+# Maintained by: Bytes Club (https://bytesclub.github.io)<bytes-club@googlegroups.com>
+# Distributed under MIT License.
+
+sudo mv ghb-auto.sh /etc/bash_completion.d/ghb
+source /etc/bash_completion.d/ghb
