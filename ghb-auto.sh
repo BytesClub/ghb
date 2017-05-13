f9b07b10c0e698c16672027bbba2404eaedd8924
Pragyan Bhattacharya <bprogyan@gmail.com>
2017-05-13 09:36:15 +0530

    Auto: Updated auto complete script
    Added auto completetion to issues/pulls on demand
    Splitted whole code in two parts
    
    Signed-Off-By: Progyan Bhattacharya <bprogyan@gmail.com>

--- ghb-auto.sh
+++ ghb-auto.sh
@@ -12,15 +12,24 @@ _ghb() {
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    readonly -a opts=("-h" "--help" "-v" "--version" "init" "status" "issues" "pulls" "open" "closed" "all" "id=")
-    opt=''
+    declare -a opts
 
+    if [[ $prev == "ghb" ]]; then
+        unset opts
+        opts=("-h" "--help" "-v" "--version" "init" "status" "issues" "pulls")
+    elif [[ $prev == "issues" || $prev == "pulls" ]]; then
+        unset opts
+        opts=("open" "closed" "all" "id=")
+    fi
+
+    opt=''
     for i in "${opts[@]}"
     do
         if [[ $i == "${cur}"* ]]; then
             opt="$opt $i"
         fi
     done
+
     COMPREPLY=( $(compgen -W "${opt}" -- ${cur}) )
     return 0
 }
