3e7b54ef45ce5a1fe28641201e8629f40e819db3
Pragyan Bhattacharya <bprogyan@gmail.com>
2017-05-13 18:22:15 +0530

    Batch: Added command prompt batch file
    Added batch file for tab completion in windows
    command prompt
    
    Signed-Off-By: Progyan Bhattacharya <bprogyan@gmail.com>

---
+++ ghb-auto.bat
@@ -0,0 +1,6 @@
+@rem command prompt completion support for GHB.
+@rem
+rem GHB - GitHub on Terminal
+rem Copyright (C) 2017 Progyan Bhattacharya <admin@codeprogyan.me>
+rem Maintained by: Bytes Club (https://bytesclub.github.io)<bytes-club@googlegroups.com>
+rem Distributed under MIT License.
